f9797a0f604f110b09c8ac70ec9b02328089adf8
Matteo Coppola <mcoppola@nic.edu>
2024-10-23 10:38:48 -0700

    added multiply

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int add(int a, int b);
+int multiply(int a, int b);
+
 int main()
 {
     cout<<"Hello world\n";
     cout<<"5 + 7 = "<<add(5,7)<<endl;
+    cout<<"5 * 7 = "<<multiply(5,7)<<endl;
     cout<<"My name is Tom\n";
     return 0;
+}
+
+int multiply(int a, int b)
+{
+    return a*b;
 }
 
 int add(int a, int b)
