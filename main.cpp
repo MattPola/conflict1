767d10305c809513640b8f63f7542c42fa182c25
Matteo Coppola <mcoppola@nic.edu>
2024-10-23 10:34:20 -0700

    added subtract function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int add(int a, int b);
+int subtract(int a, int b);
+
 int main()
 {
     cout<<"Hello world\n";
-    cout<<"5 + 7 = "<<add(5,7)<<endl;
     cout<<"My name is Tom\n";
+    cout<<"5 + 7 = "<<add(5,7)<<endl;
+    cout<<"5 - 7 = "<<subtract(5,7)<<endl;
     return 0;
+}
+
+int subtract(int a, int b)
+{
+    return a-b;
 }
 
 int add(int a, int b)
